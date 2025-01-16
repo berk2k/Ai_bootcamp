009a9ecb2910cc86f2607f7ba01e59f969c7bf7e
Berk Polat <berkpolat125@gmail.com>
2025-01-16 11:27:03 +0300

    day1

--- week1/day1_numpy.py
+++ week1/day1_numpy.py
@@ -4,4 +4,36 @@ arr = np.array([1,2,3,4])
 
 zeroes = np.zeros[(3,3)]
 
+ones = np.ones[(2,4)]
+
+range_array = np.arange(1,10,2)
+
+linspace_array = np.linspace(0,1,5)
+
+reshaped = arr.reshape((2,3))
+
+arr = np.array([1,2,3])
+
+expanded = arr[:,np.newaxis]
+
+a = np.array([1,2,3])
+
+b = np.array([4,5,6])
+
+print(a+b)
+print(a*b)
+print(a/b)
+
+arr = np.array([4,16,25])
+print(np.sqrt(arr))
+
+print(arr[1:2])
+print(arr[:2])
+reshaped = arr.reshape((2,3))
+
+matrix = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+transpose = matrix.T
+
+
 
